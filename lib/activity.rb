76b483f61342172a22bec70755f9b1d3778467d6
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-08-08 16:04:53 -0600

    first commit

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,19 @@
+require 'pry'
+
 class Activity
+
+  attr_reader :name,
+              :participants
+
+  def initialize(name, hash_of_participants)
+    @name=name
+    @participants=hash_of_participants
+    # binding.pry
+  end
+
+ def add_person(name, contribution)
+   @participants[name] = contribution
+   # binding.pry
+ end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,33 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def setup
+    @hash = {"Susi"=> 0, "Dan"=> 5, "Jordan"=> 30}
+  end
+
+  def test_it_exists
+    activity = Activity.new(name, @hash)
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_has_a_name
+    activity = Activity.new("golf", @hash)
+    assert_equal "golf", activity.name
+  end
+
+ def test_it_has_participants
+   activity = Activity.new("golf", @hash)
+   assert_equal ["Susi", "Dan", "Jordan"], activity.participants.keys
+ end
+
+def test_you_can_add_participants
+  activity = Activity.new("golf", @hash)
+  assert_equal 3, activity.participants.keys.length
+  activity.add_person("John", 20)
+  assert_equal 20, activity.participants["John"]
+  assert_equal 4, activity.participants.keys.length
+  assert_equal "John", activity.participants.keys.pop
+end
+
 end
